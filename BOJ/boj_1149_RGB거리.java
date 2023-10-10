dab6f616ace0e20fe2d6c4b10cc7cef22bfbe14d
guohsam <146626935+guohsam@users.noreply.github.com>
2023-10-10 23:10:49 +0900

    add boj_1149_RGB거리.java

---
+++ BOJ/boj_1149_RGB거리.java
@@ -0,0 +1,29 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_1149_RGB거리 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] cost = new int[N][3];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++)
+                cost[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i < N; i++) {
+            cost[i][0] += Math.min(cost[i - 1][1], cost[i - 1][2]);
+            cost[i][1] += Math.min(cost[i - 1][0], cost[i - 1][2]);
+            cost[i][2] += Math.min(cost[i - 1][0], cost[i - 1][1]);
+        }
+
+        System.out.println(Math.min(cost[N - 1][0], Math.min(cost[N - 1][1], cost[N - 1][2])));
+    }
+}
