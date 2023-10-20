aed85a74f6e1c308a423ea07f2e50f90883ac616
guohsam <146626935+guohsam@users.noreply.github.com>
2023-10-20 20:18:16 +0900

    add boj_2750_수정렬하기.java

---
+++ BOJ/boj_2750_수정렬하기.java
@@ -0,0 +1,38 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_2750_수정렬하기 {
+    public static void main(String[] args) throws IOException {
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int n = 0; n < N; n++) {
+            arr[n] = Integer.parseInt(br.readLine());
+        }
+
+        // 정렬
+//        Arrays.sort(arr);
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j < N; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        // 출력
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
