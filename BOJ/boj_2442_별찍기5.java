250fcc3f480ab01e1ed95fe43d9fea4adaa36ea6
guohsam <146626935+guohsam@users.noreply.github.com>
2023-10-29 18:42:32 +0900

    add boj_2442_별찍기5.java

---
+++ BOJ/boj_2442_별찍기5.java
@@ -0,0 +1,28 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_2442_별찍기5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= N; i++) {
+
+            for(int j = i; j < N; j++) {
+                sb.append(" ");
+            }
+
+            for(int k = 0; k < i*2-1; k++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
