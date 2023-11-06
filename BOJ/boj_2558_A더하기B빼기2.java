ac4ca3bfd863f4b91062e390c60504be668d3b91
guohsam <146626935+guohsam@users.noreply.github.com>
2023-11-06 15:31:17 +0900

    add boj_2558_A더하기B빼기2.java

---
+++ BOJ/boj_2558_A더하기B빼기2.java
@@ -0,0 +1,13 @@
+package BOJ;
+
+import java.io.*;
+
+public class boj_2558_A더하기B빼기2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int ans = Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine());
+
+        System.out.println(ans);
+    }
+}
