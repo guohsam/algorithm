b84e456a58b9c91b195f5cdd4d4d74984a9babb0
guohsam <146626935+guohsam@users.noreply.github.com>
2023-11-11 16:59:40 +0900

    add boj_10172_개.java

---
+++ BOJ/boj_10172_개.java
@@ -0,0 +1,12 @@
+package BOJ;
+
+public class boj_10172_개 {
+    public static void main(String[] args) {
+
+        System.out.println("|\\_/|\n" +
+                           "|q p|   /}\n" +
+                           "( 0 )\"\"\"\\\n" +
+                           "|\"^\"`    |\n" +
+                           "||_/=\\\\__|");
+    }
+}
