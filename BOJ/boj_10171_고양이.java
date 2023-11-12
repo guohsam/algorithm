3888ce1435a16d41564520ac5bad4bd5a9c2bd45
guohsam <146626935+guohsam@users.noreply.github.com>
2023-11-12 19:25:52 +0900

    add boj_10171_고양이.java

---
+++ BOJ/boj_10171_고양이.java
@@ -0,0 +1,11 @@
+package BOJ;
+
+public class boj_10171_고양이 {
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\\n" +
+                           " )  ( ')\n" +
+                           "(  /  )\n" +
+                           " \\(__)|");
+    }
+}
