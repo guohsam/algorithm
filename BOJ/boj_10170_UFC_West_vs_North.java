ca28d83bc2258be72b6bb3e5ce72b13727180f45
guohsam <146626935+guohsam@users.noreply.github.com>
2023-11-13 21:18:58 +0900

    add boj_10170_UFC_West_vs_North.java

---
+++ BOJ/boj_10170_UFC_West_vs_North.java
@@ -0,0 +1,22 @@
+package BOJ;
+
+public class boj_10170_UFC_West_vs_North {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("NFC West       W   L  T\n" +
+                "-----------------------\n" +
+                "Seattle        13  3  0\n" +
+                "San Francisco  12  4  0\n" +
+                "Arizona        10  6  0\n" +
+                "St. Louis      7   9  0\n" +
+                "\n" +
+                "NFC North      W   L  T\n" +
+                "-----------------------\n" +
+                "Green Bay      8   7  1\n" +
+                "Chicago        8   8  0\n" +
+                "Detroit        7   9  0\n" +
+                "Minnesota      5  10  1");
+
+        System.out.println(sb);
+    }
+}
+
