5d127a7625372b5a7eefe24967e7d55719e5beff
guohsam <146626935+guohsam@users.noreply.github.com>
2023-11-14 22:43:27 +0900

    add boj_10699_오늘날짜.java

---
+++ BOJ/boj_10699_오늘날짜.java
@@ -0,0 +1,14 @@
+package BOJ;
+
+import java.time.LocalDate;
+
+public class boj_10699_오늘날짜 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+
+        System.out.println(now);
+    }
+
+}
+
+
