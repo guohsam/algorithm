138c671e47464bb9d24c69c4771c0791451c2bdb
guohsam <146626935+guohsam@users.noreply.github.com>
2023-11-17 18:13:32 +0900

    add boj_2393_rook.java

---
+++ BOJ/boj_2393_rook.java
@@ -0,0 +1,20 @@
+package BOJ;
+
+public class boj_2393_rook {
+    public static void main(String[] args) {
+
+        System.out.println("  ___  ___  ___\n" +
+                           "  | |__| |__| |\n" +
+                           "  |           |\n" +
+                           "   \\_________/\n" +
+                           "    \\_______/\n" +
+                           "     |     |\n" +
+                           "     |     |\n" +
+                           "     |     |\n" +
+                           "     |     |\n" +
+                           "     |_____|\n" +
+                           "  __/       \\__\n" +
+                           " /             \\\n" +
+                           "/_______________\\");
+    }
+}
