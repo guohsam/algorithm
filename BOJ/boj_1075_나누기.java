79bba9874d09dfaee1df58855bfcb63daf9db2f8
guohsam <146626935+guohsam@users.noreply.github.com>
2023-12-11 07:53:26 +0900

    add boj_1075_나누기.java

---
+++ BOJ/boj_1075_나누기.java
@@ -0,0 +1,25 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_1075_나누기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int F = Integer.parseInt(br.readLine());
+
+        N -= (N % 100);
+
+        while(true) {
+            if(N % F == 0) {
+                break;
+            }
+            N++;
+        }
+        System.out.format("%02d", (N % 100));
+    }
+}
