be848801c3c74776e7afc3e4e574cd5158130775
guohsam <146626935+guohsam@users.noreply.github.com>
2023-12-17 22:30:34 +0900

    add boj_1837_암호제작.java

---
+++ BOJ/boj_1837_암호제작.java
@@ -0,0 +1,35 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_1837_암호제작 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger p = new BigInteger(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+
+        boolean[] primes = new boolean[k + 1];
+        primes[1] = true;
+
+        for(int i = 2 ; i < k ; i++){
+            if(primes[i]) continue;
+            BigInteger now = new BigInteger(Integer.toString(i));
+            if(p.mod(now).compareTo(BigInteger.ZERO) == 0) { 
+                System.out.println("BAD " + now);
+                return;
+            }
+            for(int j = i + i ; j <= k ; j += i){
+                primes[j] = true;
+            }
+        }
+        System.out.println("GOOD");
+
+    }
+}
