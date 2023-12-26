cf734954faefcea8c280aca17a2f5a13a53866e5
guohsam <146626935+guohsam@users.noreply.github.com>
2023-12-26 16:11:03 +0900

    add boj_2562_최댓값.java

---
+++ BOJ/boj_2562_최댓값.java
@@ -0,0 +1,25 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_2562_최댓값 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int arr[] = new int[9];
+        int max = arr[0];
+        int index = 0;
+      
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            if (max < arr[i]) {
+                max = arr[i];
+                index = i+1;
+            }
+        }
+      
+        System.out.println(max);
+        System.out.println(index);
+    }
+}
