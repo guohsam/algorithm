44bc2f7a885c0adfc07bf495bcdcb1c1bd5f1692
guohsam <146626935+guohsam@users.noreply.github.com>
2023-12-29 22:05:39 +0900

    add boj_10189_HOOK.java

---
+++ BOJ/boj_10189_HOOK.java
@@ -0,0 +1,11 @@
+package BOJ;
+
+public class boj_10189_HOOK {
+    public static void main(String[] args) {
+
+        System.out.println("#  # #### #### #  #\n" +
+                           "#### #  # #  # # #\n" +
+                           "#### #  # #  # # #\n" +
+                           "#  # #### #### #  #");
+    }
+}
