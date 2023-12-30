ae9dfe142ee2e7f65ac9a808b8f4b43dd3596f0d
guohsam <146626935+guohsam@users.noreply.github.com>
2023-12-30 17:30:20 +0900

    add boj_10926.java

---
+++ BOJ/boj_10926.java
@@ -0,0 +1,16 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(br.readLine()).append("??!");
+
+        System.out.println(sb);
+    }
+}
